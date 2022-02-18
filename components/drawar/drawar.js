ce4f354fba88339409d96d5b7b00a48d48ebc6c8
Rohan Mondal <mondalrohan108@gmail.com>
2022-02-19 02:19:29 +0530

    updated drawar

--- components/drawar/drawar.js
+++ components/drawar/drawar.js
@@ -1,13 +1,24 @@
-const menuBtn=document.querySelector(".drawar-menu-btn");
-const drawar=document.querySelector(".drawar-container");
-const closeBtn=document.querySelector(".drawar-close");
-
-menuBtn.addEventListener('click',()=>{
-    drawar.classList.add("drawer-trans-on");
-    drawar.classList.remove("drawer-trans-off");
-});
-
-closeBtn.addEventListener('click',()=>{
+const menuBtn = document.querySelector(".drawar-menu-btn");
+const drawar = document.querySelector(".drawar-container");
+const closeBtn = document.querySelector(".drawar-close");
+document.onclick = (e) => {
+  const classList = e.target.classList;
+  console.log(classList);
+  if (
+    !classList.contains("drawar-container") &&
+    !classList.contains("fa-bars")
+  ) {
     drawar.classList.add("drawer-trans-off");
     drawar.classList.remove("drawer-trans-on");
-})
+  }
+};
+
+menuBtn.addEventListener("click", () => {
+  drawar.classList.add("drawer-trans-on");
+  drawar.classList.remove("drawer-trans-off");
+});
+
+closeBtn.addEventListener("click", () => {
+  drawar.classList.add("drawer-trans-off");
+  drawar.classList.remove("drawer-trans-on");
+});
